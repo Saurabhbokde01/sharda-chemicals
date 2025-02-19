f3684553b77fb73d33fbd386f0b95bfdfe7c875a
saurabh <bokadesaurabh88@gmail.com>
2025-02-19 17:16:45 +0530

    fouth commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,7 +8,9 @@ const Home = () => {
   const [charIndex, setCharIndex] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [showCursor, setShowCursor] = useState(true);
+  const [activeIndex, setActiveIndex] = useState(0); // For paragraph switching
 
+  // Typing Effect
   useEffect(() => {
     const typingSpeed = 250; // Set typing and deleting speed to 250ms
 
@@ -31,13 +33,22 @@ const Home = () => {
     return () => clearTimeout(typingEffect);
   }, [currentText, isDeleting]);
 
-  // Blinking cursor effect
+  // Blinking Cursor Effect
   useEffect(() => {
     const cursorBlink = setInterval(() => {
       setShowCursor((prev) => !prev);
     }, 500);
     return () => clearInterval(cursorBlink);
   }, []);
+
+  // Switch Between Paragraphs
+  const handleNext = () => {
+    setActiveIndex((prev) => (prev === 1 ? 0 : prev + 1));
+  };
+
+  const handlePrev = () => {
+    setActiveIndex((prev) => (prev === 0 ? 1 : prev - 1));
+  };
 
   return (
     <div className="home">
@@ -52,12 +63,16 @@ const Home = () => {
           </p>
         </div>
         <div className="desc">
-          <p>
+          <p className={activeIndex === 0 ? "active" : ""}>
             Sharda Chemicals is a certified company that specializes in the manufacturing, supply, and distribution of various Pre and Post construction chemicals. To keep up with the flawlessness of the assortment, we procure supreme quality constituents from our dependable and trustworthy vendors. It is because of the quality and flawlessness offered that our chemicals are widely applauded in the construction industries.
           </p>
-        </div>
-        <div className="product-btn">
-          <button>View Products</button>
+          <p className={activeIndex === 1 ? "active" : ""}>
+            Sharda Chemicals is the leading manufacturer and supplier of superior construction chemicals. Sharda Chemicals manufactures a wide range of products which are supplied to some of the biggest construction companies and industries. These construction chemicals are widely used in the construction of industrial, commercial, and infrastructural projects.
+          </p>
+          <div className="desc-btn">
+            <button className="pre-btn" onClick={handlePrev}>{"<"}</button>
+            <button className="next-btn" onClick={handleNext}>{">"}</button>
+          </div>
         </div>
       </div>
     </div>
