9927061f4aba593e945b219543faca9095f66c84
saurabh <bokadesaurabh88@gmail.com>
2025-02-22 11:04:56 +0530

    eighteenth commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faEnvelope, faMobileScreenButton } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot, faEnvelope, faMobileScreenButton, faHeadphonesSimple, faPaperPlane, faRotateRight} from "@fortawesome/free-solid-svg-icons";
 import "./Contact.css";
 
 const Contact = () => {
@@ -8,7 +8,7 @@ const Contact = () => {
       {/* Left Side */}
       <div className="contact-left">
         <div className="contact-heading">
-          <h2>Contact Us</h2>
+          <h2>Contact Us <span><FontAwesomeIcon icon={faHeadphonesSimple} bounce style={{color: "#0c2757",}} /></span></h2>
           <p>
             For industry-leading construction chemicals with exceptional
             performance, Sharda Chemical is your trusted partner.
@@ -34,41 +34,41 @@ const Contact = () => {
 
           <div className="contact-form-group textarea-group">
             <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" placeholder="please define your query"></textarea>
+            <textarea id="message" name="message" placeholder="Please define your query"></textarea>
           </div>
 
           <div className="contact-buttons">
             <button type="submit" className="submit-btn">
-              Submit <span>📩</span>
+              Submit <span><FontAwesomeIcon icon={faPaperPlane} /></span>
             </button>
             <button type="reset" className="reset-btn">
-              Reset <span>↻</span>
+              Reset <span><FontAwesomeIcon icon={faRotateRight} spin /></span>
             </button>
           </div>
         </form>
       </div>
 
-      {/* Right Side */}
+      {/* Right Side - Contact Info */}
       <div className="contact-right">
-        <div className="contact-info">
-          <div className="contact-icon">
-            <FontAwesomeIcon icon={faLocationDot} />
+        <div className="contact-info-box">
+          <div className="contact-info-header">
+            <FontAwesomeIcon icon={faLocationDot} flip className="contact-icon" />
             <h4>Company Location</h4>
           </div>
           <p>Plot no 29, Old subhedar layout, Sharda Square, Nagpur-440024</p>
         </div>
 
-        <div className="contact-info">
-          <div className="contact-icon">
-            <FontAwesomeIcon icon={faEnvelope} />
+        <div className="contact-info-box">
+          <div className="contact-info-header">
+            <FontAwesomeIcon icon={faEnvelope} beatFade className="contact-icon" />
             <h4>Email</h4>
           </div>
           <p>shardachemicals1@gmail.com</p>
         </div>
 
-        <div className="contact-info">
-          <div className="contact-icon">
-            <FontAwesomeIcon icon={faMobileScreenButton} />
+        <div className="contact-info-box">
+          <div className="contact-info-header">
+            <FontAwesomeIcon icon={faMobileScreenButton} shake className="contact-icon" />
             <h4>Phone Numbers</h4>
           </div>
           <p>+91 9423476195<br />+91 7083892200</p>
