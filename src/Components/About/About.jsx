112c95e54aa87602214027da9d2f677377fd5dbb
saurabh <bokadesaurabh88@gmail.com>
2025-02-24 09:07:17 +0530

    28th commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import img1 from "../../assets/about-img-1.webp";
-import img2 from "../../assets/about-img-2.webp";
+import img1 from "../../assets/about-img-5.jpg";
+import img2 from "../../assets/about-img-6.webp";
 import "./About.css";
 
 const About = () => {
@@ -18,9 +18,7 @@ const About = () => {
           <h2 className="about-title">About Sharda Chemicals</h2>
           <img src={img1} alt="Company operations" className="about-img" />
           <p className="about-text">
-            Sharda Chemicals is a certified company specializing in high-quality
-            Pre and Post-construction chemicals. We ensure excellence by sourcing
-            top-tier constituents from trusted vendors.
+          Sharda Chemicals is a certified company that specializes in the manufacturing, supply, and distribution of various Pre and Post construction chemicals. To keep up with the flawlessness of the assortment, we procure supreme quality constituents from our dependable and trustworthy vendors. It is because of the quality and flawlessness offered that our chemicals are widely applauded in the construction industries.
           </p>
         </div>
 
@@ -28,9 +26,7 @@ const About = () => {
           <h2 className="about-title">What We Can Do?</h2>
           <img src={img2} alt="Our expertise" className="about-img" />
           <p className="about-text about-text2">
-            As a leading manufacturer, we supply superior construction chemicals
-            to major industrial and infrastructural projects, ensuring
-            cutting-edge solutions for modern construction demands.
+          Sharda Chemicals is the leading manufacturer and supplier of superior construction chemicals. Sharda Chemicals manufactures a wide range of products which are supplied to some of the biggest construction companies and industries. These construction chemicals are widely used in the construction of industrial, commercial, and infrastructural projects.
           </p>
         </div>
 
