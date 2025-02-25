2717dc34f5aafe23e205c0e1b04c6580d17bf234
saurabh <bokadesaurabh88@gmail.com>
2025-02-25 14:15:34 +0530

    32th commit

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -4,32 +4,39 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Products.css";
 
-import p1_img1 from "../../assets/products/paver.webp";
-import p1_img2 from "../../assets/products/paver2.webp";
-import p1_img3 from "../../assets/products/paver3.webp";
-import p1_img4 from "../../assets/products/paver4.jpg";
+import p1_img1 from "../../assets/product/paver1.webp";
+import p1_img2 from "../../assets/product/paver2.webp";
+import p1_img3 from "../../assets/product/paver3.webp";
 
-import p2_img1 from "../../assets/products/poles1.jpg";
-import p2_img2 from "../../assets/products/poles2.webp";
-import p2_img3 from "../../assets/products/poles3.webp";
+import p2_img1 from "../../assets/product/pipes1.webp";
+import p2_img2 from "../../assets/product/pipes2.webp";
+import p2_img3 from "../../assets/product/pipes3.webp";
 
-import p3_img1 from "../../assets/products/divider.webp";
-import p3_img2 from "../../assets/products/divider2.jpg";
-import p3_img3 from "../../assets/products/divider3.avif";
-import p3_img4 from "../../assets/products/divider4.jpg";
+import p3_img1 from "../../assets/product/stone1.webp";
+import p3_img2 from "../../assets/product/stone2.webp";
+import p3_img3 from "../../assets/product/stone3.webp";
 
-import p4_img1 from "../../assets/products/mold.jpg";
+import p4_img1 from "../../assets/product/mold1.webp";
+import p4_img2 from "../../assets/product/mold2.webp";
 
-import p5_img1 from "../../assets/products/oxide.jpg";
-import p5_img2 from "../../assets/products/oxide2.jpg";
+import p5_img1 from "../../assets/product/oxide1.webp";
+import p5_img2 from "../../assets/product/oxide2.webp";
 
-import p6_img1 from "../../assets/products/coat.jfif";
+import p6_img1 from "../../assets/product/coat1.webp";
+import p6_img2 from "../../assets/product/coat2.webp";
+
+import p7_img1 from "../../assets/product/pc1.webp";
+import p7_img2 from "../../assets/product/pc2.webp";
+import p7_img3 from "../../assets/product/pc3.webp";
+import p7_img4 from "../../assets/product/pc4.webp";
+import p7_img5 from "../../assets/product/pc5.webp";
+import p7_img6 from "../../assets/product/pc6.webp";
 
 const products = [
   {
     id: 1,
     title: "1. Hardener for Paver & Block",
-    images: [p1_img1, p1_img2, p1_img3, p1_img4],
+    images: [p1_img1, p1_img2, p1_img3],
     details: [
       "Greed M-70 to M-80.",
       "It takes 7 to 8 hours for setting after filling mold.",
@@ -41,25 +48,35 @@ const products = [
     title: "2. Hardener for RCC Pipes, Fencing Poles & RCC Electricity Poles",
     images: [p2_img1, p2_img2, p2_img3],
     details: [
-      "Greed M-70 to M-80.",
-      "It takes 7 to 8 hours for setting after filling mold.",
-      "Expires in 2 years from manufacturing.",
     ],
   },
   {
     id: 3,
     title: "3. Hardener for Crub Stone, MileStone & Road Divider",
-    images: [p3_img1, p3_img2, p3_img3, p3_img4],
+    images: [p3_img1, p3_img2, p3_img3],
     details: [
-      "Provides superior binding strength.",
-      "Ideal for high-load areas like curbstones.",
-      "Ensures long-lasting durability.",
     ],
   },
   {
     id: 4,
-    title: "4. Mold Release Agent",
-    images: [p4_img1],
+    title: "4. Surface Coating",
+    images: [p6_img1, p6_img2],
+    details: [
+      "In 1 liter, it covers 100-120 sq. feet area.",
+      "Expires in 6 months from manufacturing.",
+    ],
+  },
+  {
+    id: 5,
+    title: "5. pc base melamine hardener for paver block, rcc pipes, fencing poles, rcc electricity poles, concrete floor and cement rodes",
+    images: [p7_img1, p7_img2, p7_img3, p7_img4, p7_img5, p7_img6],
+    details: [
+    ],
+  },
+  {
+    id: 6,
+    title: "6. Mold Release Agent",
+    images: [p4_img1, p4_img2],
     details: [
       "In 1 liter of Mold release agent, 4 liters of water should be added.",
       "Prevents concrete from sticking to molds.",
@@ -67,22 +84,13 @@ const products = [
     ],
   },
   {
-    id: 5,
-    title: "5. Colour-Oxide",
+    id: 7,
+    title: "7. Colour-Oxide",
     images: [p5_img1, p5_img2],
     details: [
       "Red - 25 KG Packing.",
       "Yellow - 25 KG Packing.",
       "Black - 25 KG Packing.",
-    ],
-  },
-  {
-    id: 6,
-    title: "6. Surface Coating",
-    images: [p6_img1],
-    details: [
-      "In 1 liter, it covers 100-120 sq. feet area.",
-      "Expires in 6 months from manufacturing.",
     ],
   },
 ];
